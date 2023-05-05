0018825db44840215bdf235a43a0f795bfe96bfa
aswinpmony <aswinpmony@gmail.com>
2023-05-05 11:19:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import NavBa from './Compone/NavBa/NavBa';
+import { action, comedy, originals,horror,documentaries,romance } from './urls'
+import './App.css'
+import Banner from './Compone/Banner/Banner';
+import RowPost from './Compone/RowPoster/RowPost';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <NavBa/>
+      <Banner/>
+      <RowPost url={originals}  title='Netflix Originals'/>
+      <RowPost url={action}  title='Action' isSmall />
+      <RowPost url={comedy}  title='Comedy' isSmall />
+      <RowPost url={horror}  title='Horror' isSmall />
+      <RowPost url={romance}  title='Romantic' isSmall />
+      <RowPost url={documentaries}  title='Documentaries' isSmall />
+      
+      
     </div>
-  );
+  )
 }
 
 export default App;
+
